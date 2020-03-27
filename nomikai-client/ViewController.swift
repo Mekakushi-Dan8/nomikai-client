ea0f13374667d8d7c17fc0bd55dc9f2bf3201a3f
1motio <>
2020-03-27 20:50:33 +0900

    initial commit

---
+++ nomikai-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nomikai-client
+//
+//  Created by 本村 on 2020/03/27.
+//  Copyright © 2020 Mekakushi-Dan8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
